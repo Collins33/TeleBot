7ac64c2253f53488bd8bda12b660e804f305493f
Collins njau <collinsnjau39@gmail.com>
2017-12-28 23:31:36 +0300

    I have added the method to parse the json response into python dictionary using json.loads method

--- echobot.py
+++ echobot.py
@@ -4,9 +4,16 @@ import requests
 
 URL="https://api.telegram.org/bot{}/getme".format(TelegramToken)#url to build the request
 
+#make a request to telegram using the created url
 def get_url(url):
     response=requests.get(url)
     content= response.content.decode("utf8")
-    print (content)
+    return content
 
-get_url(URL)
+#parse the result into python dictionary
+def get_json_url(url):
+    content=get_url(url)
+    js=json.loads(content)
+    print (js)
+
+get_json_url(URL)
