0b89bb1224aa9fbfdb2863c134571649d20d145c
Collins njau <collinsnjau39@gmail.com>
2017-12-28 23:24:29 +0300

    I have added the function to make a request to telegram and return details about the bot

--- echobot.py
+++ echobot.py
@@ -2,4 +2,11 @@ import json
 from configuration import *
 import requests
 
-URL="https://api.telegram.org/bot{}/".format(TelegramToken)#url to build the request
+URL="https://api.telegram.org/bot{}/getme".format(TelegramToken)#url to build the request
+
+def get_url(url):
+    response=requests.get(url)
+    content= response.content.decode("utf8")
+    print (content)
+
+get_url(URL)
