818c85c6be6e609464e29a872b41fdaf1f3cea05
Collins njau <collinsnjau39@gmail.com>
2018-01-04 19:13:03 +0300

    I have added the method to create a table in the db and create a column in the table

--- dbhelper.py
+++ dbhelper.py
@@ -1,6 +1,13 @@
 import sqlite3
 
 class DBHelper:
+    #this method takes a database name as argument and connects to it
     def __init__(self,dbname="todo.sqlite"):
         self.dbname=dbname
         self.conn=sqlite3.connect(dbname)
+
+    #this method creates a table called items with column called description
+    def setUp(self):
+        stmt="CREATE TABLE IF NOT EXISTS items (description text)"
+        self.conn.execute(stmt)
+        self.conn.commit()   
